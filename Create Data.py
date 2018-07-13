1cb639c16483a8f92ed344b0d0b585f680ee0d06
dataguix <37068939+dataguix@users.noreply.github.com>
2018-07-13 12:00:16 +0200

    Update Create Data.py

--- Create Data.py
+++ Create Data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 import sys
 import os
-
+import multiprocessing
 #Convert pdf to text
 import textract
 import PyPDF2
@@ -107,8 +107,40 @@ def CreateData(directoryPath):
         
     df = pd.DataFrame(dataDic)
     
-    df.to_csv('data.csv')
+    df.to_csv(directoryPath + 'data.csv')
     print('\n CSV created')
     
     
+
+if __name__ == '__main__':
+    start = time.time()
+    p1 = multiprocessing.Process(target=CreateData, args=['C:/report1'])
+    p2 = multiprocessing.Process(target=CreateData, args=['C:/report2'])
+    p3 = multiprocessing.Process(target=CreateData, args=['C:/report3'])
+    p4 = multiprocessing.Process(target=CreateData, args=['C:/report4'])
+    p5 = multiprocessing.Process(target=CreateData, args=['C:/report5'])
+    p6 = multiprocessing.Process(target=CreateData, args=['C:/report6'])
+    p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
+    p5.start()
+    p6.start()
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+    p5.join()
+    p6.join()
+    print(time.time()-start)
+ 
+combined_csv = pd.DataFrame([])
+for fn in os.listdir('C:/'):
+    if re.match(r"^.*.csv$", fn):
+        df=pd.read_csv('C:/'+fn)
+        combined_csv = combined_csv.append(df)
+combined_csv.index = range(len(combined_csv))    
+del df
+
+
 CreateData('Annual reports')
